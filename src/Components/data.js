795e61b4c34c988fe1f15ab97bfeec6e6682932c
Thiru02 <thiruparanb@gmail.com>
2022-12-16 22:32:07 +0530

    portfolio initialization

---
+++ src/Components/data.js
@@ -0,0 +1,39 @@
+import box_office from '../Images/BoxOffice.png'
+import tic_tac_tac from '../Images/TicaTacToe.png'
+import eats_app from '../Images/EatsApp.png'
+import to_do_list from '../Images/ToDoList.png'
+
+export const projects = [
+    {
+      title: 'Box Office',
+      description: 'React',
+      image: box_office,
+      link: 'https://thiru02.github.io/Box-office-latest/#/',
+    },
+    {
+      title: 'Tic Tac Toe',
+      description: 'React',
+      image: tic_tac_tac,
+      link: 'https://amazinggametictactoe.surge.sh/',
+    },
+    {
+      title: 'Eats App',
+      description: 'React',
+      image: eats_app,
+      link: 'https://thiru02.github.io/Eats-app/',
+    },
+    {
+      title: 'To Do List',
+      description: 'React',
+      image: to_do_list,
+      link: 'https://thiru02.github.io/To-Do-List/',
+    },
+  ];
+
+  export const skills =[
+    'React', 'HTML/CSS'
+  ]
+
+  export const testimonials = [
+    'dfd','dfd'
+  ]
